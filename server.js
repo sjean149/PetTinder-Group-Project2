d3dedfd4df49247e720f8ba2773260c12a195f9a
SHMERNIE <fredony.ernesto@gmail.com>
2024-07-15 18:47:21 -0700

    added sessions, rewrote code for userSeeds, fixed front login code, created an api for user login, and etc.

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const path = require('path');
 const express = require('express');
-//const session = require('express-session');
+const session = require('express-session');
 
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 //const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({});
 
-/*
+
 const sess = {
     secret: 'Super secret secret',
     cookie: {},
@@ -24,9 +26,9 @@ const sess = {
   };
 
 app.use(session(sess));
-*/
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
