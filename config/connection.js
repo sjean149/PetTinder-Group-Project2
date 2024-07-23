e804e67807543c5d8d62d6cd6db7200e09ebdb98
fredonyernesto <124623921+fredonyernesto@users.noreply.github.com>
2024-07-22 17:53:42 -0700

    Revert "connect to postgresql"

--- config/connection.js
+++ config/connection.js
@@ -3,17 +3,19 @@ require('dotenv').config();
 
 let sequelize;
 
+// Check for database URL in environment variables
 if (process.env.DB_URL) {
-  sequelize = new Sequelize(process.env.DB_URL);
+  sequelize = new Sequelize(process.env.DB_URL); 
 } else {
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
+    process.env.DB_NAME,     
+    process.env.DB_USER,     
+    process.env.DB_PASSWORD, 
     {
-      host: 'localhost',
+      host: 'localhost',     
       dialect: 'postgres',
-    },
+      port:5432    
+    }
   );
 }
 
